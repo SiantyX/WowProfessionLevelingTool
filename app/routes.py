279d0d739e0bc13f61b68996d27158a7d94d7995
Felix Sandvik <siantyx@gmail.com>
2022-05-08 16:57:44 +0000

    Updated displayed version number to 0.2.2

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from app.forms import UserInputForm
 from datetime import datetime
 from app.functions import *
 
-VERSION="0.2.1"
+VERSION="0.2.2"
 
 author = {
     'username': 'Original by Znibb, difficulty by ItsMonkk. Merged by SiantyX',
